b58b36a7da66ae70bfacf145c769dd23c688a3d8
omidsdgi <omid69sdgi@gmail.com>
2025-09-09 11:38:55 +0330

    Apply GlobalStyles with Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import GlobalStyles from "./styles/GlobalStyles.js";
+import styled from "styled-components";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
 
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    background-color: yellow;
+`
+const StyledApp=styled.main`
+    background-color: orangered;
+    padding:20px;
+
+`
 function App() {
 
-      const x=0
-  return (
- <div>
-Hello World! Mehraneh
- </div>
-  )
+    return (
+        <>
+            <GlobalStyles/>
+            <StyledApp>
+                <H1>The Wild Oasis </H1>
+                <Button>Check in</Button>
+                <Button>Check out</Button>
+                <Input type="number" placeholder="Number of guests" />
+                <Input type="number" placeholder="Number of guests" />
+            </StyledApp>
+        </>
+    )
 }
 
 export default App
