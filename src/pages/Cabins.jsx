b62a84ff69d9aeda39aaa3e4941ca590a5c68769
omidsdgi <omid69sdgi@gmail.com>
2025-09-13 19:44:04 +0330

    Add React hook form (createCabinForm)

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,21 +1,24 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable.jsx";
-
+import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
+import {useState} from "react";
+import Button from "../ui/Button.jsx";
 
 function Cabins() {
-
-  return (
-      <>
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>Filter / Sort</p>
-    </Row>
-<Row>
-    <CabinTable/>
-</Row>
-      </>
-  );
+    const [showForm, setShowForm] = useState(false)
+    return (
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">All cabins</Heading>
+                <p>Filter / Sort</p>
+            </Row>
+            <Row>
+                <CabinTable/>
+                <Button onClick={() => setShowForm(!showForm)}>Add new cabin</Button>
+                {showForm && <CreateCabinForm/>}
+            </Row>
+        </>
+    );
 }
-
 export default Cabins;
