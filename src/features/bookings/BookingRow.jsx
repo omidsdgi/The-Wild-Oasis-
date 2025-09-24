f2c7767c453b8e39cc33a1c2ed12352fecf5b874
omidsdgi <omid69sdgi@gmail.com>
2025-09-24 20:23:38 +0330

    apply building the single Booking page

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -6,6 +6,9 @@ import Table from "../../ui/Table";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
+import Menus from "../../ui/Menus.jsx";
+import {HiEye} from "react-icons/hi";
+import {useNavigate} from "react-router-dom";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -30,24 +33,25 @@ const Stacked = styled.div`
 `;
 
 const Amount = styled.div`
-  font-family: "Sono";
+  font-family: "Sono", sans-serif;
   font-weight: 500;
 `;
 
 function BookingRow({
   booking: {
     id: bookingId,
-    created_at,
+    // created_at,
     startDate,
     endDate,
     numNights,
-    numGuests,
+    // numGuests,
     totalPrice,
     status,
     guests: { fullName: guestName, email },
     cabins: { name: cabinName },
   },
 }) {
+    const navigate=useNavigate();
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
@@ -79,6 +83,15 @@ function BookingRow({
       <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
+
+        <Menus.Menu>
+            <Menus.Toggle id={bookingId} />
+            <Menus.List id={bookingId}>
+            <Menus.Button icon={<HiEye />} onClick={()=>navigate(`/bookings/${bookingId}`)}>
+                See details
+            </Menus.Button>
+            </Menus.List>
+        </Menus.Menu>
     </Table.Row>
   );
 }
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -74,7 +74,7 @@ html {
 }
 
 body {
-  font-family: "Poppins", sans-serif;
+  font-family: "Poppins", "Sono",sans-serif;
   color: var(--color-grey-700);
 
   transition: color 0.3s, background-color 0.3s;
